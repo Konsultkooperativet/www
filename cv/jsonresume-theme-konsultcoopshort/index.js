68d77bb102564cae9bcb8065da92e954da470ce9
mberglundmx <33635893+mberglundmx@users.noreply.github.com>
2017-12-28 20:23:22 +0100

    removed partials from short cv

--- cv/jsonresume-theme-konsultcoopshort/index.js
+++ cv/jsonresume-theme-konsultcoopshort/index.js
@@ -23,17 +23,6 @@ Handlebars.registerHelper('DMY', DMY);
 function render(resume) {
   const css = readFileSync(`${__dirname}/style.css`, 'utf-8');
   const tpl = readFileSync(`${__dirname}/resume.hbs`, 'utf-8');
-  const partialsDir = join(__dirname, 'theme/partials');
-  const filenames = readdirSync(partialsDir);
-
-  filenames.forEach((filename) => {
-    const matches = /^([^.]+).hbs$/.exec(filename);
-    if (!matches) return;
-    const name = matches[1];
-    const filepath = join(partialsDir, filename);
-    const template = readFileSync(filepath, 'utf8');
-    Handlebars.registerPartial(name, template);
-  });
 
   return Handlebars.compile(tpl)({
     css,
