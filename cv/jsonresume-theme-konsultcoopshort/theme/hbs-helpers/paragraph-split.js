d1c0d57bb36c97d234712ecb1c56935f26853dc9
mberglundmx <33635893+mberglundmx@users.noreply.github.com>
2017-12-29 11:53:19 +0100

    updating summary text

--- cv/jsonresume-theme-konsultcoopshort/theme/hbs-helpers/paragraph-split.js
+++ cv/jsonresume-theme-konsultcoopshort/theme/hbs-helpers/paragraph-split.js
@@ -3,7 +3,7 @@ const { SafeString } = require('handlebars');
 const paragraphSplit = (text) => {
   const expr = /\r\n|\r|\n/g;
   const lines = Array.isArray(text) ? text.join('').split(expr) : text.split(expr);
-  const output = lines.filter(line => line).reduce((a, b) => `${a}<p>${b}</p>`, '');
+  const output = lines.filter(line => line).reduce((a, b) => `${a}<p class='presentation small'>${b}</p>`, '');
   return new SafeString(output);
 };
 
